f262fa6e78036d18a961e6bb93aa24e74d782c5e
gmongaras <gabriel@mongaras.com>
2022-04-19 17:13:23 -0500

    Added Darknet-53

--- src/YOLOX.py
+++ src/YOLOX.py
@@ -1,13 +1,105 @@
+from ast import Delete
 import torch
 from torch import nn
 
 
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
 
 
 
 
 
 
+class Darknet53(nn.Module):
+    # Initialize the mode
+    # Inputs:
+    #   inSize - The number of input channels (3 for RGB)
+    def __init__(self, inSize=3):
+        super(Darknet53, self).__init__()
+        
+        # Create the model blocks
+        self.block1 = nn.Sequential(
+            nn.Conv2d(inSize, 32, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+        ).to(device)
+        self.block2 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=1),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        ).to(device)
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+        ).to(device)
+        self.block4 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=1),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+        ).to(device)
+        self.block5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+        ).to(device)
+        self.block6 = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+        ).to(device)
+        self.block7 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+        ).to(device)
+        self.block8 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+        ).to(device)
+        self.block9 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),
+        ).to(device)
+        self.block10 = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=1),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+        ).to(device)
+        self.globalPool = nn.AdaptiveAvgPool2d(1)
+        self.connected = nn.Linear(1024, 1000)
+        self.soft = nn.Softmax(0)
+        
+    
+    # Get a prediction from the model
+    def forward(self, X):
+        v = self.block1(X)
+        
+        res = torch.clone(v)
+        v = self.block2(v)+res
+        v = self.block3(v)
+        
+        res = torch.clone(v)
+        v = self.block4(v)+res
+        res = torch.clone(v)
+        v = self.block4(v)+res
+        
+        v = self.block5(v)
+        
+        for i in range(0, 8):
+            res = torch.clone(v)
+            v = self.block6(v)+res
+        
+        v = self.block7(v)
+        
+        for i in range(0, 8):
+            res = torch.clone(v)
+            v = self.block8(v)+res
+        
+        v = self.block9(v)
+        
+        for i in range(0, 4):
+            res = torch.clone(v)
+            v = self.block10(v)+res
+        del res
+        
+        v = torch.squeeze(self.globalPool(v))
+        
+        v = self.connected(v)
+        
+        v = self.soft(v)
+        
+        return v
 
 
 
@@ -16,7 +108,29 @@ from torch import nn
 
 
 class YOLOX(nn.Module):
-    def __init__(self):
+    # Initialze the model
+    # Inputs:
+    #   k - The number of possible bounding boxes
+    #   numEpochs - The number of epochs to train the model for
+    #   batchSize - The size of each minibatch
+    #   warmupEpochs - Number of warmup epochs to train the model for
+    #   lr_init - Initial learning rate
+    #   weightDecay - Amount to decay weights over time
+    #   momentum - Momentum of the SGD optimizer
+    def __init__(self, k, numEpochs, batchSize, warmupEpochs, lr_init, weightDecay, momentum):
         super(YOLOX, self).__init__()
         
-        print()
+        # Save the model paramters
+        self.k = k
+        self.numEpochs = numEpochs
+        self.batchSize = batchSize
+        self.warmupEpochs = warmupEpochs
+        self.lr_init = lr_init
+        
+        # Create the optimizer
+        self.optim = torch.optim.SGD(self.parameters, lr=lr_init, momentum=momentum, weight_decay=weightDecay)
+    
+    
+    
+    def forward(self):
+        return 1
--- src/main.py
+++ src/main.py
@@ -4,9 +4,16 @@ import numpy as np
 import skimage.io as io
 from PIL import Image
 import random
+import torch
+
+from YOLOX import Darknet53
 
 
 
+
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
 
 
 
@@ -17,6 +24,12 @@ import random
 def main():
     # Model Hyperparameters
     k = 10                  # The max number of annotations per image
+    numEpochs = 300         # The number of epochs to train the model for
+    batchSize = 128         # The size of each minibatch
+    warmupEpochs = 5        # Number of warmup epochs to train the model for
+    lr_init = 0.01          # Initial learning rate
+    weightDecay = 0.0005    # Amount to decay weights over time
+    momentum = 0.9          # Momentum of the SGD optimizer
     
     
     # COCO dataset parameters
@@ -24,8 +37,8 @@ def main():
     dataType = "val2017"    # The type of data being used in the COCO dataset
     annFile = '{}/annotations/instances_{}.json'.format(dataDir,dataType)
     categories = ["person", "dog", "cat"]   # The categories to load in
-    numToLoad = 10          # Max Number of data images to load in (use -1 for all)
-    resize = 512            # Resize the images to a quare pixel value (can be 1024, 512, or 256)
+    numToLoad = 2           # Max Number of data images to load in (use -1 for all)
+    resize = 256            # Resize the images to a quare pixel value (can be 1024, 512, or 256)
     
     
     
@@ -37,13 +50,19 @@ def main():
     
     # Get the image data and annotations
     img_data = []
+    category_Ids = dict()
     ann_data = dict()
     seen = []
     for c in categories:
+        # Get all images in the category c
         catIds = coco.getCatIds(catNms=[c])
         imgIds = coco.getImgIds(catIds=catIds)
         sub_img_data = coco.loadImgs(imgIds)
         
+        # Save the category ids
+        category_Ids[c] = catIds[0]
+        
+        # Get the new images
         notSeen = []
         for i in sub_img_data:
             if i['id'] not in seen:
@@ -51,6 +70,7 @@ def main():
                 notSeen.append(i)
         img_data += notSeen
         
+        # Save the annotations for the new images
         for i in notSeen:
             ann_data[i['id']] = coco.loadAnns(coco.getAnnIds(imgIds=i['id'], iscrowd=None))
     
@@ -68,24 +88,32 @@ def main():
         img = img.convert("RGB")
         img = img.resize((resize, resize))
         imgs.append(np.array(img))
-    imgs = np.array(imgs)
+    imgs = torch.tensor(np.array(imgs), dtype=torch.float, requires_grad=False, device=device)
+    
+    # Correction so that channels are first, not last
+    if imgs.shape[-1] == 3:
+        imgs = torch.reshape(imgs, (imgs.shape[0], 3, imgs.shape[1], imgs.shape[2]))
     print("Images Loaded")
     
     
-    # Turn the annotations into a better form
+    # Get all annoations which we want
     anns = []
     for img_d in img_data:
-        ann = ann_data[img_d["id"]][:k]
+        ann = ann_data[img_d["id"]]
         ann_bbox = []
         for a in ann:
-            ann_bbox.append(a["bbox"])
-        anns.append(ann_bbox)
+            # Save the annotation if it bounds the wanted ID
+            if a["category_id"] in category_Ids.values():
+                ann_bbox.append(a["bbox"])
+        anns.append(ann_bbox[:k])
     
     
     
     
     ### Model Training
-    model = YOLOX()
+    model = Darknet53()
+    a = model.forward(imgs[0])
+    #model = YOLOX(k, numEpochs, batchSize, warmupEpochs, lr_init, weightDecay, momentum)
     
     
     
